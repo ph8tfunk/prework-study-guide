2b299fee3768caadd04fb1bbbb4775540580eb4f
Tom Paling <137108048+ph8tfunk@users.noreply.github.com>
2023-09-22 10:45:47 +0100

    Added JS to the project

---
+++ assets/scripts.js
@@ -0,0 +1,19 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let index = 0; index < topics.length; index++) {
+    console.log(topics[index]);
+    
+}
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
